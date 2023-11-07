07d1c4f20062cb588ca6d91d5f73bc848bbaead9
Vladyslav Diachenko <basher207vd@gmail.com>
2023-11-07 19:33:25 +0200

    Added grouping functionality to statistics.js

--- statistics.js
+++ statistics.js
@@ -1,5 +1,18 @@
 const { groupBy, mergeMap, reduce, map } = require("rxjs");
 
 module.exports = function getGroupStatistics(students$) {
-  // Your code here
+  return students$.pipe(
+    groupBy(p => p.group),
+    mergeMap(group$ => group$.pipe(
+      reduce((acc, cur) => {
+        const ratingSum = acc.ratingSum + cur.rating;
+        const groupMembers = ++acc.members;
+        return {...acc, ratingSum: ratingSum, members: groupMembers};
+      },{ group: group$.key, ratingSum: 0, members: 0})
+    )),
+    map(groupObj => ({
+      group: groupObj.group,
+      avgRating: groupObj.ratingSum / groupObj.members
+    }))
+  );
 };
